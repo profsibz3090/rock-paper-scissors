c10add2d815b1159bbe0b4f35d607ddeddd2040e
Donty Sibanda <sibandadonty@gmail.com>
2023-07-02 11:02:20 +0200

    initial ccommit

--- game.js
+++ game.js
@@ -1,3 +1,14 @@
+let win = 0;
+let lose = 0;
+let tied = 0;
+let round = 0;
+
+document.querySelector('#rock').addEventListener('click', ()=> playRound('rock', getComputerChoice()));
+document.querySelector('#paper').addEventListener('click', ()=> playRound('paper', getComputerChoice()));
+document.querySelector('#scissors').addEventListener('click', ()=> playRound('scissors', getComputerChoice()));
+
+const resultDisplayElement = document.createElement('p');
+const scoreDisplayElement = document.createElement('p');
 
 function getComputerChoice() {
     const rand = (Math.random() * 10);
@@ -11,6 +22,19 @@ function getComputerChoice() {
     }
 
     return move;
+}
+
+
+
+function gameFinished() {
+    scoreDisplayElement.innerText = tied === 5? `you tied with ${win} win/wins ${lose} lose/loses and ${tied} ties`  :win > lose? 
+    `you are the overall winner with ${win} win/wins ${lose} lose/loses and ${tied} ties`
+  : `computer is the overall winner with ${win} win/wins ${lose} lose/loses and ${tied} ties`;
+    lose = 0;
+   win = 0;
+   tied = 0;
+   round = 0;
+   document.querySelector('.results').appendChild(scoreDisplayElement);
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -40,13 +64,25 @@ function playRound(playerSelection, computerSelection) {
              result = "You Tied! Scissors equals Scissors";
           }
     }
+    round++;
+    round === 5 && gameFinished();  
+    // determineWinner(result);
+    if(result.includes('You Win')) {
+        win++;
+     } else if(result.includes('You Lose')){
+         lose++;
+     } else if(result.includes('You Tied')) {
+         tied++;
+     }
+    resultDisplayElement.innerText = result;
+    document.querySelector('.results').appendChild(resultDisplayElement);
     return result;
 }
 
 function game() {
-    let win = 0;
-    let lose = 0;
-    let tied = 0;
+    // let win = 0;
+    // let lose = 0;
+    // let tied = 0;
     for (let index = 0; index < 5; index++) {
         const computerSelection = getComputerChoice();
         const playerSelection = prompt("pick a move(rock, paper, scissors)", "rock");
@@ -65,7 +101,9 @@ function game() {
                       : `computer is the overall winner with ${win} win/wins ${lose} lose/loses and ${tied} ties`;
 }
 
+
 function determineWinner(result) {
+
     if(result.includes('you win')) {
        win++;
     } else if(result.includes('you lose')){
@@ -74,5 +112,3 @@ function determineWinner(result) {
         tied++;
     }
 }
-
-console.log(game());
